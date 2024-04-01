900a1451e88a191cbd7a895811f439e160907f11
Jase017 <lyang11@uw.edu>
2024-04-01 00:12:03 -0700

    version1

--- app.py
+++ app.py
@@ -1,15 +1,48 @@
 import streamlit as st
 
-st.title("🙌Hi, my name is :blue[Liu Yang],")
+st.balloons()
+
+st.title("🙌Hi, my name is :blue[Liu Yang]")
 st.title("Here is my profile page :sunglasses:")
 
-st.header("I'm a human")
-st.image("./images/lion.jpg")
+col1, col2= st.columns(2)
 
-st.header("About Me 👨‍🎤")
-st.markdown("""
-            -School:
-            -Major:
-            -Skill:
+with col1:
+    st.image("./images/photo.jpg")
+with col2:
+    st.header("I'm a :red[UI & UX] Designer")
+    st.header("About Me 👨‍🎤")
+    st.markdown("""
+            - School📙:
+                - Beijing University of Technology
+                - University of Washington
+            - Major 🛠: 
+                - Industrial Design
+                - UI & UX Design
+            - Skill:  
+                - 3D Modeling
+                - Prototyping
+                - User Research
 
 """)
+col3, col4 = st.columns(2)
+with col3:
+    st.header(":rainbow[Work Experience]")  
+    st.markdown("""
+- Luckin Coffee ☕
+- Madame Tussauds Beijing 🎓
+- Net Ease 👔              
+""")
+with col4:
+    st.header(":rainbow[Hobby & Interest]")  
+    st.markdown("""
+- Soccer ⚽
+- Video Game 🎮
+- Dog 🐶   
+""")
+
+st.header("Interesting Projects")
+st.header(":orange[OOK]")  
+st.image("./images/OOK.png")
+st.header(":blue[IMASAZ]")  
+st.image("./images/photo1.png")
